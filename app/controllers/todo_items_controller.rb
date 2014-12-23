0c218b12b952f667dd8c7f728dfd5bc8a6687e02
Lafwind <lafwind@gmail.com>
2014-12-24 00:32:27 +0800

    完成todo_item的complete功能

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -16,6 +16,13 @@ class TodoItemsController < ApplicationController
     end
   end
 
+  def update
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list
+  end
+
   def destroy
     @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])

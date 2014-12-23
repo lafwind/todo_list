aed88ff976a9e9590427e914bde1933a44c48962
Lafwind <lafwind@gmail.com>
2014-12-23 17:34:27 +0800

    生成todo_lists controller，并设置其index为root_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'todo_lists#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

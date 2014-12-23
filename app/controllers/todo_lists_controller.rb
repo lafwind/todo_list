f245ce596377addd4770f02618537df15f7e560a
Lafwind <lafwind@gmail.com>
2014-12-23 21:02:38 +0800

    实现新建todo list功能

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,4 +1,26 @@
 class TodoListsController < ApplicationController
   def index
+    @todo_lists = TodoList.all
   end
+
+  def new
+    @todo_list = TodoList.new
+  end
+
+  def create
+    @todo_list = TodoList.create(todo_list_params)
+    if @todo_list.save
+      flash[:success] = "Well done!"
+      redirect_to todo_lists_path
+    else
+      flash[:error] = "Some errors here!"
+      render 'new'
+    end
+  end
+
+    private
+
+    def todo_list_params
+      params[:todo_list].permit(:title, :description)
+    end
 end
